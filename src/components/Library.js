21d72000ddbcb8a737cea3150678c60f4b1a4ba2
modelwifeofamerica <modelwifeofamerica@gmail.com>
2018-12-26 14:39:58 -0500

    Bloc Jams Component Library Ckpt 2757

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
  class Library extends Component { 
+   constructor(props) {
+     super(props);
+     this.state = { albums: albumData };
+   }
    render() {
    return ( 
       <section className='library'>
-        Library will go here
+        {
+          this.state.albums.map( (album, index) => 
+            <Link to={`/album/${album.slug}`} key={index}>
+               <img src={album.albumCover} alt={album.title} />
+               <div>{album.title}</div>
+               <div>{album.artist}</div>
+               <div>{album.songs.length} songs</div>
+          )
+        }
       </section>
       /* Turning a component into a class allows us to control the state*/
      );
